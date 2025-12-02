d0258c4030e0f166483590481431c4d87c188cc0
AnnPoshtak <annposhtak@gmail.com>
2025-12-02 20:37:36 +0200

    Update task1_part1.cpp

--- c++/task1/task1_part1.cpp
+++ c++/task1/task1_part1.cpp
@@ -2,29 +2,28 @@
 using namespace std;
 
 int main(){
-	vector<string> array = {"L68","L30","R48","L5","R60","L55","L1","L99","R14","L82"};
+        vector<string> array = {"L68","L30","R48","L5","R60","L55","L1","L99","R14","L82"};
 
-	long long count0 = 0;
+        long long count0 = 0;
 
-	int currentNumber = 50;
+        int currentNumber = 50;
 
-	for (long long i = 0; i < array.size(); i++){
+        for (long long i = 0; i < array.size(); i++){
   char direction = array[i][0];
-		long long number = stoll(array[i);
+                long long number = stoll(array[i].substr(1));
 
-		if (direction == 'L'){
-			currentNumber = abs(currentNumber-number) % 100;
-		} else{
-			currentNumber = (currentNumber+number) % 100;
-		}
+                if (direction == 'L'){                     currentNumber = ((currentNumber - number) % 100 + 100) % 100;
+                } else{
+                        currentNumber = (currentNumber+number) % 100;
+                }
 
-		if (currentNumber == 0){
-			count0++;
-		
-		}
-		
-	}
-	
+                if (currentNumber == 0){
+                        count0++;
 
-	cout << count0;
+                }
+
+        }
+
+
+        cout << count0;
 }
