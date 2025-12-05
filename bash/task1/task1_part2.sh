4bdd235f0a336f6755fc4073e78f8d7a8743bb85
AnnPoshtak <annposhtak@gmail.com>
2025-12-05 20:55:50 +0200

    Improve zero counting logic in task1_part2.sh
    Refactor logic for counting zeros based on direction and current number calculations.

--- bash/task1/task1_part2.sh
+++ bash/task1/task1_part2.sh
@@ -5,22 +5,34 @@ count_0=0
 current_number=50
 
 for element in "${array[@]}"; do
-	direction=${element:0:1}
-	n=${element:1}
-	number=$(( n + 0 ))
-	
+        direction=${element:0:1}
+        n=${element:1}
+        number=$(( n + 0 ))
 
-	if [[ "$direction" == "L" ]]; then
-		current_number=$(( ((current_number - number) % 100 + 100) % 100 ))
-	else
-		current_number=$(( (current_number + number) % 100 ))
-	fi
 
-	if (( current_number == 0 )); then
-		count_0=$((count_0+1))
-	fi	
+        if [[ "$direction" == "L" ]]; then
+                count_0=$((count_0 + number / 100))
+                rest=$(( number % 100  ))
+
+                c=$((current_number - rest))
+
+                if (( current_number > 0 && c <= 0  )); then
+                        count_0=$((count_0+1))
+                fi
+
+                current_number=$(( ((current_number - number) % 100 + 100) % 100 ))
+        else
+                count_0=$((count_0 + number / 100))
+                rest=$(( number%100  ))
+                c=$((current_number+rest))
+
+                if (( current_number > 0 && c > 99  )); then
+                        count_0=$((count_0+1))
+                fi
+
+                current_number=$(( (current_number + number) % 100 ))
+        fi
+
 
 done
 echo "Answer: ${count_0}"
-
-
